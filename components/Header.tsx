af6409de71f9d670e5f46dda29d003b105c89c10
jhigger <njohnjhigger@gmail.com>
2022-10-20 12:06:56 +0800

    edit

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,13 +5,13 @@ type Props = {};
 
 const Header = (props: Props) => {
 	return (
-		<div className="flex justify-between text-white pb-12 items-center">
+		<header className="flex justify-between text-white pb-12 items-center">
 			<Image src={Logo} alt="logo" width={180} height={32} />
 			<div className="text-sm flex flex-col items-end">
 				<p>Last Update</p>
 				<p>10/19/2022, 6:05:42 AM</p>
 			</div>
-		</div>
+		</header>
 	);
 };
 
