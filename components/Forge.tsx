dc293b0e692b99e59c4692b6bdb45fb43e15d794
jhigger <njohnjhigger@gmail.com>
2022-10-21 12:34:54 +0800

    changed image

--- components/Forge.tsx
+++ components/Forge.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import Logo from '../assets/BSL_WHITE.png';
+import Mercury from '../assets/Mercury.png';
 import CardGrid, { CardGridProps } from './CardGrid';
 import Section, { SectionHeader } from './Section';
 
@@ -9,7 +9,9 @@ const Forge = ({ forgeStats }: ForgeProps) => {
 	return (
 		<>
 			<SectionHeader
-				image={<Image src={Logo} alt="logo" width={135} height={24} />}
+				image={
+					<Image src={Mercury} alt="logo" width={32} height={32} />
+				}
 				title="Forge"
 			/>
 			<Section>
--- components/Mercury.tsx
+++ components/Mercury.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import Logo from '../assets/BSL_WHITE.png';
+import MercuryImage from '../assets/Mercury.png';
 import CardGrid, { CardGridProps } from './CardGrid';
 import Section, { SectionHeader } from './Section';
 
@@ -9,7 +9,14 @@ const Mercury = ({ mercuryStats }: MercuryProps) => {
 	return (
 		<>
 			<SectionHeader
-				image={<Image src={Logo} alt="logo" width={135} height={24} />}
+				image={
+					<Image
+						src={MercuryImage}
+						alt="logo"
+						width={32}
+						height={32}
+					/>
+				}
 				title="Mercury"
 			/>
 			<Section>
