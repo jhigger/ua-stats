6ad90c61d53e70ed21bdb6c118f3eee4d70c1c8e
jhigger <njohnjhigger@gmail.com>
2023-01-29 17:10:53 +0800

    add external icon

--- components/CardGrid.tsx
+++ components/CardGrid.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import Image from 'next/image';
 import Card from './Card';
+import { FaExternalLinkAlt } from 'react-icons/fa';
 
 export type CardGridProps = {
 	title: string;
@@ -14,21 +15,37 @@ const CardGrid = ({ items }: { items: CardGridProps[] }) => {
 			{items.map(({ title, data, project }, i) => {
 				return (
 					<Card key={title + i}>
-						<dt className="text-sm font-medium text-neutral-300 truncate">
-							{title}
-						</dt>
+						{project ? (
+							<dt className="flex justify-between text-sm font-medium text-neutral-300 truncate">
+								{title}
+
+								<a
+									href={project.link}
+									target="_blank"
+									rel="noreferrer"
+								>
+									<FaExternalLinkAlt color="white" />
+								</a>
+							</dt>
+						) : (
+							<dt className="text-sm font-medium text-neutral-300 truncate">
+								{title}
+							</dt>
+						)}
 						<dd className="mt-1 text-3xl font-semibold text-neutral-100">
 							{data}
 						</dd>
 						{project ? (
-							<a
-								href={project.link}
-								target="_blank"
-								rel="noreferrer"
-								className="min-h-[12rem] flex items-center justify-center"
-							>
-								<img src={project.image} alt="card image" />
-							</a>
+							<div>
+								<a
+									href={project.link}
+									target="_blank"
+									rel="noreferrer"
+									className="min-h-[12rem] flex flex-col items-center justify-center"
+								>
+									<img src={project.image} alt="card image" />
+								</a>
+							</div>
 						) : (
 							''
 						)}
